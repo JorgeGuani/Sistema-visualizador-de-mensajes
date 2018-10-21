d794cc098a5eca64798ab338a611a70be6775bde
Jorge Guani <jorge_guani@hotmail.com>
2018-10-21 15:21:39 -0500

    Agregado sensor humedad y autoscroll

--- sistemaVisualizadorMensajes.ino
+++ sistemaVisualizadorMensajes.ino
@@ -2,8 +2,11 @@
  * Librería libre para conexión entre java y arduino
  * https://github.com/PanamaHitek/PanamaHitek_Arduino/releases/tag/3.0.0
  */
+//Librerías usadas
+#include <LiquidCrystal.h> //LCD
+#include "DHT.h"           //Sensor de humedad
 
-#include <LiquidCrystal.h>
+//Pines de alimentación del LCD
 #define RS 3
 #define E  5
 #define D4 6
@@ -11,24 +14,36 @@
 #define D6 10
 #define D7 11
 
-#define sensortemperatura 0
+//Variables para el sensor de humedad
+#define DHTPIN 2          //Entrada digital 2
+#define DHTTYPE DHT11     //Tipo de sensor DHT
+DHT dht(DHTPIN, DHTTYPE); //Comunicación con el sensor
+float valorHumedad = 0;
+
+//Variables para el sensor de temperatura
+#define sensortemperatura 0  //Entrada analógica 0
 float voltaje = 0;
 float valorTemperatura = 0;
 
-const long A = 1000; //Resistencia en oscuridad en KΩ
-const int B = 15; //Resistencia a la luz (10 Lux) en KΩ
-const int Rc = 10; //Resistencia calibracion en KΩ
-const int PINLUZ = A1;
-int valorLuzAnalogica;
-int iluminacion;
+//Variables para el sensor de luminosidad
+const long A = 1000;      //Resistencia en oscuridad en KΩ
+const int B = 15;         //Resistencia a la luz (10 Lux) en KΩ
+const int Rc = 10;        //Resistencia calibracion en KΩ
+const int PINLUZ = A1;    //Entrada analógica 1
+int valorAnalogicoLuz = 0;
+int valorLuminosidad = 0;
 
+//Variables para el control de las entradas seriales
 int input;
 boolean esClima;
 
+//Indicarle al LCD sus pines de alimentación
 LiquidCrystal lcd(RS,E,D4,D5,D6,D7);
 
+
 void setup() {
-  lcd.begin(16,2);
+  lcd.begin(16,1);
+  dht.begin();
   Serial.begin(9600);
 }
 
@@ -57,9 +72,13 @@ void loop() {
 
     //En caso de que la entrada no sea un estado climatológico,
     //Quiere decir que corresponde a un mensaje:
-    if(!esClima) {               
+    if(!esClima) {
+      lcd.begin(16,1);
+      lcd.setCursor(16,0);
       while(Serial.available() > 0) {
-        lcd.write(Serial.read());  
+        lcd.autoscroll();
+        lcd.write(Serial.read());  //Escribe el mensaje en el LCD
+        delay(275);
       }
     }  
   }
@@ -68,52 +87,51 @@ void loop() {
 
 //MÉTODOS PARA CALCULAR ESTADOS CLIMATOLÓGICOS:
 void mostrarTemperatura() {
+  lcd.begin(16,2);
+  lcd.noAutoscroll();
   while(input == '1'){
     voltaje = analogRead(sensortemperatura)*3.3/1023;
     valorTemperatura = voltaje*100;
     lcd.home();
-    lcd.print("Temperatura: ");
+    lcd.print("Temperatura:");
     lcd.setCursor(0,1);
     lcd.print(valorTemperatura);
     lcd.print("C");
     Serial.println(valorTemperatura);
     delay(1000);
-    //nuevaEntrada();
-    //input = Serial.read();
-    //Serial.println(input);
-    if(Serial.read() != 10) break;
-        
+    if(Serial.read() != 10) break; //Si se detecta un cambio en la entrada serial      
   }
 }
 
 void mostrarHumedad() {
+  lcd.begin(16,2);
+  lcd.noAutoscroll();
   while(input == '2'){
+    valorHumedad = dht.readHumidity(); //Se lee la humedad
     lcd.home();
-    lcd.write("hum");
-    nuevaEntrada();
+    lcd.print("Humedad:");
+    lcd.setCursor(0,1);
+    lcd.print(valorHumedad);
+    Serial.println("Humedad: ");
+    Serial.println(valorHumedad);
+    delay(1000);
+    if(Serial.read() != 10) break; //Si se detecta un cambio en la entrada serial      
   }
 }
 
 void mostrarLuminosidad() {
+  lcd.begin(16,2);
+  lcd.noAutoscroll();
   while(input == '3'){
-    valorLuzAnalogica = analogRead(PINLUZ);
-    iluminacion = ((long)valorLuzAnalogica*A*10)/
-                  ((long)B*Rc*(1024-valorLuzAnalogica));
-    lcd.clear();
+    valorAnalogicoLuz = analogRead(PINLUZ);
+    valorLuminosidad = ((long)valorAnalogicoLuz*A*10)/
+                  ((long)B*Rc*(1024-valorAnalogicoLuz));
     lcd.home();
     lcd.print("luminosidad: ");
     lcd.setCursor(0,1);
-    lcd.print(iluminacion);
+    lcd.print(valorLuminosidad);
     lcd.print("  ");
     delay(1000);
     if(Serial.read() != 10) break;
-  }
-}
-
-//Sirve para detectar si se ingresa una nueva entrada
-void nuevaEntrada() {
-  if(Serial.available() > 0) {
-    input = Serial.read();
-    Serial.println(input);    
   }
 }
